a7f05b8a51e19fe372e31a69bee4a6153038c3a6
Mohammad Adiban <69257287+mohammad-adiban@users.noreply.github.com>
2023-02-01 11:57:45 +0100

    Add files via upload

---
+++ web/public/scripts/multiselect.js
@@ -0,0 +1,37 @@
+﻿//multiselect collection
+window.multiselects = [];
+
+if (typeof ($) != 'undefined') {
+	//jQuery extension for multiselect
+	$.fn.multiselect = function () {
+		//if no elements passed - it is not an error
+		if (this.length != 0) {
+			$(this).each(function (i, e) {
+				window.multiselects.push(new Multiselect($(e)));
+			});
+		}
+	};
+	
+	$(document).click(function (event) {
+		hideMultiselects(event);
+	});
+} else {
+	document.multiselect = function(selector) {
+		m_helper.each(document.querySelectorAll(selector), function(e) {
+			window.multiselects.push(new Multiselect(e));
+		});
+	}
+	
+	window.onclick = function(event) {
+		hideMultiselects(event);
+	};
+}
+
+function hideMultiselects(event) {
+	m_helper.each(window.multiselects, function(e) {
+		if (document.getElementById(e._getItemListIdentifier()).offsetParent &&
+				!m_helper.parent(event.target, e._getIdentifier())) {
+			e._hideList(e, event);
+		}
+	});
+}
